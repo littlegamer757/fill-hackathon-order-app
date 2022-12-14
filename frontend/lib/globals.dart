092b72fbc81d24fb02e5021cf3667a63cd7d6da9
Lea <seifried.lea@gmail.com>
2022-12-14 14:26:35 +0100

    Implement start page

---
+++ frontend/lib/globals.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+var bigRegular = TextStyle(
+    fontSize: 12
+);
+
+var bigBold = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 50
+);
+
+var bigBoldRed = TextStyle(
+  color: Color.fromRGBO(203, 69, 95, 80),
+    fontWeight: FontWeight.bold,
+    fontSize: 24
+);
+
+var bigBoldWhite = TextStyle(
+    color: Colors.white,
+    fontWeight: FontWeight.bold,
+    fontSize: 24
+);
+
+const filliRed = Color.fromARGB(255, 203, 69, 95);
+
+const filliLightRed = Color.fromARGB(255, 220, 181, 189);
--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:fill_hackathon/globals.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:simple_shadow/simple_shadow.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,6 +26,8 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
+        fontFamily: 'ProductSans',
+        scaffoldBackgroundColor: Color.fromARGB(255, 203, 69, 95)
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -70,46 +75,112 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      // appBar: AppBar(
+      //   // Here we take the value from the MyHomePage object that was created by
+      //   // the App.build method, and use it to set our appbar title.
+      //   title: Text(widget.title),
+      // ),
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            bottom: -200,
+            child: SimpleShadow(
+              opacity: 0.6,
+              color: Colors.black,
+              offset: Offset(5,5),
+              sigma: 7,
+              child: SvgPicture.asset(
+                'assets/splash.svg',
+                alignment: Alignment.bottomCenter,
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,),
+            )),
+          Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Invoke "debug painting" (press "p" in the console, choose the
+            // "Toggle Debug Paint" action from the Flutter Inspector in Android
+            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+            // to see the wireframe for each widget.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.only(top: 100, left: 35),
+                alignment: Alignment.centerLeft,
+                child: Column (
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text('Get the newest ', style: TextStyle(
+                        fontSize: 28,
+                      color: Color.fromARGB(255, 255, 208, 217)
+                    )),
+                    Text('Filli Future ', style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 40,
+                    )),
+
+                  ],
+                ),
+              ),
+              Expanded(
+                  child: Container(
+                    alignment: Alignment.bottomCenter,
+                    child: SvgPicture.asset(
+                      'assets/BG-Filli.svg',
+                      width: MediaQuery.of(context).size.width / 1.7,
+                      fit: BoxFit.scaleDown,),
+                  )
+              ),
+              Container(
+                margin: const EdgeInsets.only(bottom: 50.0, top: 25.0),
+                child: ElevatedButton.icon(
+                  label: const Text('Order', style: TextStyle(
+                      fontSize: 20
+                  ),),
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(28.0)
+                        )
+                    ),
+                    backgroundColor: MaterialStateProperty.all<Color>(filliRed),
+                  ).merge(ElevatedButton.styleFrom(
+                      minimumSize: const Size(150, 56)
+                  )),
+                  onPressed: () {},
+                  icon: const Icon( // <-- Icon
+                    Icons.shopping_cart_outlined,
+                    size: 26.0,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // floatingActionButton: FloatingActionButton.extended(
+      //     label: Text('Order'), // <-- Text
+      //     backgroundColor: Color.fromRGBO(203, 69, 95, 80),
+      //     icon: Icon( // <-- Icon
+      //       Icons.shopping_cart_outlined,
+      //       size: 24.0,
+      //     ),
+      //     onPressed: () {},
+      //   ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
